a2c6263ea34067bd154edbd166381c8962d8c6ee
lixingDEVIL <1379470308@qq.com>
2020-10-18 07:59:12 +0800

    new

---
+++ build/precache-manifest.56c9c5e8393f1c431f0ce69eb94d2b62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89caa770a3b8a4909ada49b6203c578e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e0ec56fe938ea925ad",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "62bfaa9bee5b5e13a9b0",
+    "url": "/static/css/main.059fca7b.chunk.css"
+  },
+  {
+    "revision": "66e0ec56fe938ea925ad",
+    "url": "/static/js/2.b4c3fbbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4c3fbbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bfaa9bee5b5e13a9b0",
+    "url": "/static/js/main.bd313981.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
